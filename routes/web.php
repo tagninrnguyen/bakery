9b0f8339541ae14e8018d94a08e2c0f6e5a7150d
TagNinr_Nguyen <tagninr.nguyen@gmail.com>
2018-05-30 15:14:45 +0700

    Update file routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['as' => 'frontend', 'namespace' => 'Frontend'], function(){
+	Route::get('/', 'HomeController@index');
 });
